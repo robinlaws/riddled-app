e310331e1a905e95f0e61cecc4fdc486c3f50071
robinlaws <94911535+robinlaws@users.noreply.github.com>
2022-06-08 15:18:47 -0230

    merged
    fixed merge errors

--- client/src/pages/Stats.js
+++ client/src/pages/Stats.js
@@ -1,22 +1,17 @@
 import React from "react";
 
-export function Stats(props){      
-    console.log(props.user);
+export function Stats(){      
+    let user = JSON.parse(localStorage.getItem('user'));
     return(
         <>
-        <h1>GAMES PLAYED: {props.user.gamesPlayed}</h1>
-        <h1>GAMES WON: {props.user.wins}</h1>
+        <h1>GAMES PLAYED: {user.gamesPlayed}</h1>
+        <h1>GAMES WON: {user.wins}</h1>
         <h1>GUESS DISTRIBUTION: </h1>
-        <h2>One: {props.user.one}</h2>
-        <h2>Two: {props.user.two}</h2>
-        <h2>Three: {props.user.three}</h2>
-        <h2>Four: {props.user.four}</h2>
-        <h2>Five: {props.user.five}</h2>
+        <h2>One: {user.one}</h2>
+        <h2>Two: {user.two}</h2>
+        <h2>Three: {user.three}</h2>
+        <h2>Four: {user.four}</h2>
+        <h2>Five: {user.five}</h2>
         </>
     )       
 }
-
-
-
-
-   
